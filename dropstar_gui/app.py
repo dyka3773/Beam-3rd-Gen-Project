cd793d3d9e69448b9d2eff4b1bf4db3f7a1d5021
Hercules Konsoulas <dyka3773@gmail.com>
2023-04-22 16:23:41 +0300

    Created a lightweight project using Flask

---
+++ dropstar_gui/app.py
@@ -0,0 +1,29 @@
+import datetime
+from flask import Flask, render_template, url_for
+
+from controllers import home_page, downlink_page, uplink_page
+
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+@app.route('/')
+def index():
+    return home_page.render()
+
+@app.route('/downlink/')
+def downlink():
+    return downlink_page.render()
+
+@app.route('/uplink/')
+def uplink():
+    return uplink_page.render()
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run(
+        host="0.0.0.0", # This will make the app available to other computers on the network
+        port="8000"
+    )
