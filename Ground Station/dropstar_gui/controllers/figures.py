1ca9aa96d3f0e8f61d8583b217815556fc923e74
Hercules Konsoulas <dyka3773@gmail.com>
2023-06-30 21:28:47 +0300

    Squashed everything into the main branch

---
+++ Ground Station/dropstar_gui/controllers/figures.py
@@ -0,0 +1,88 @@
+import io
+import numpy as np
+import matplotlib
+
+from matplotlib import pyplot as plt
+
+from .utils import base64_util
+from .status import get_temperature, get_pressure
+
+matplotlib.use('agg')
+
+
+FIGURES = ["temperature", "pressure"]
+
+def get_temp_plot(img: io.BytesIO) -> None:
+    """Creates a matplotlib plot and stores it in the given io buffer.
+
+    Args:
+        img (io.BytesIO): A buffer to store the plot as an image.
+    """
+    time_range_of_plot = 60 # This value determines the time range of the plot
+    
+    sensor1 = get_temperature("sensor1", time_range_of_plot)
+    sensor2 = get_temperature("sensor2", time_range_of_plot)
+    
+    x_1 = [i/3 for i in range(len(sensor1))]
+    x_2 = [i/3 for i in range(len(sensor2))]
+    
+    figure, ax = plt.subplots()
+    ax.plot(x_1,sensor1)
+    ax.plot(x_2,sensor2)
+    ax.set_title("Temperature Plot")
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Temperature (C)")
+    ax.grid()
+    figure.savefig(img, format='png')
+    plt.close(figure)
+
+def get_pressure_plot(img: io.BytesIO) -> None:
+    """Creates a matplotlib plot and stores it in the given io buffer.
+    
+    Args:
+        img (io.BytesIO): A buffer to store the plot as an image.
+    """
+    time_range_of_plot = 60 # This value determines the time range of the plot
+    
+    sensor1 = get_pressure("sensor1", time_range_of_plot)
+    sensor2 = get_pressure("sensor2", time_range_of_plot)
+    
+    x_1 = [i/3 for i in range(len(sensor1))]
+    x_2 = [i/3 for i in range(len(sensor2))]
+    
+    
+    figure, ax = plt.subplots()
+    ax.plot(x_1,sensor1)
+    ax.plot(x_2,sensor2)
+    ax.set_title("Pressure Plot")
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Pressure (atm)")
+    ax.grid()
+    figure.savefig(img, format='png')
+    plt.close(figure)
+
+def get_plot_by_type(type: str) -> str:
+    """Returns a base64 encoded image of a matplotlib plot.
+    
+    Args:
+        type (str): The type of plot to be returned. Available types are:
+            - temperature
+            - pressure
+    
+    Returns:
+        str: A base64 encoded image of a matplotlib plot.
+    """
+    img = io.BytesIO()
+    
+    if type == "temperature":
+        get_temp_plot(img)
+    elif type == "pressure":
+        get_pressure_plot(img)
+    else:
+        raise ValueError("Invalid figure type")
+    
+    img.seek(0)
+    
+    encoded_image = base64_util.encode_image(img.getvalue())
+    
+    return f"{encoded_image}"
---
+++ Ground Station/dropstar_gui/controllers/status.py
@@ -0,0 +1,157 @@
+import numpy as np
+import sqlite3 as sql
+import logging
+import time
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# The following functions are used to get the status of each component of the system
+
+def get_motor_speed() -> int:
+    """Gets the motor speed from the db.
+
+    Returns:
+        int: The motor speed.
+    """
+    with sql.connect('GS_data.db') as db:
+        speed = db.execute('SELECT motor_speed FROM GS_data ORDER BY time DESC LIMIT 1').fetchone()[0]
+        
+    return speed
+
+def get_sound_card_status() -> bool:
+    """Gets the sound card status from the db.
+
+    Returns:
+        bool: The sound card status.
+    """
+    with sql.connect('GS_data.db') as db:
+        status = db.execute('SELECT sound_card_status FROM GS_data ORDER BY time DESC LIMIT 1').fetchone()[0]
+    
+    return status
+
+def get_camera_status() -> bool:
+    """Gets the camera status from the db.
+
+    Returns:
+        bool: The camera status.
+    """
+    with sql.connect('GS_data.db') as db:
+        status = db.execute('SELECT camera_status FROM GS_data ORDER BY time DESC LIMIT 1').fetchone()[0]
+    
+    return status
+
+def get_heater_status() -> bool:
+    """Gets the heater status from the db.
+
+    Returns:
+        bool: The heater status.
+    """
+    with sql.connect('GS_data.db') as db:
+        status = db.execute('SELECT heater_status FROM GS_data ORDER BY time DESC LIMIT 1').fetchone()[0]
+    
+    return status
+
+def get_temperature(sensor: str, time: int) -> list:
+    """Gets the temperature from the db.
+
+    Args:
+        sensor (str): The sensor from which the temperature will be read.
+        time (int): The time for which the temperature will be read.
+
+    Returns:
+        list: The temperature values for the given time.
+    """
+    with sql.connect('GS_data.db') as db:
+        if sensor == 'sensor1':
+            col = 'temp_1'
+        elif sensor == 'sensor2':
+            col = 'temp_2'
+        else:
+            raise ValueError('Invalid sensor name')
+        
+        temperature = db.execute(f'''SELECT {col} 
+                                        FROM GS_data
+                                        WHERE time >= DATETIME('now', '-{time} seconds')
+                                        ORDER BY time DESC
+                                ''').fetchall()
+    
+    temperature.reverse()
+    logging.debug(f'Got temperature: {temperature}')
+    
+    return temperature
+
+def get_pressure(sensor: str, time: int) -> list:
+    """Gets the pressure from the db.
+
+    Args:
+        sensor (str): The sensor from which the pressure will be read.
+        time (int): The time for which the pressure will be read.
+
+    Returns:
+        list: The pressure values for the given time.
+    """
+    with sql.connect('GS_data.db') as db:
+        if sensor == 'sensor1':
+            col = 'pressure_1'
+        elif sensor == 'sensor2':
+            col = 'pressure_2'
+        else:
+            raise ValueError('Invalid sensor name')
+        
+        pressure = db.execute(f'''SELECT {col} 
+                                    FROM GS_data
+                                    WHERE time >= DATETIME('now', '-{time} seconds')
+                                    ORDER BY time DESC
+                                ''').fetchall()
+    
+    pressure.reverse()
+    logging.debug(f'Got pressure: {pressure}')
+    
+    return pressure
+
+
+# The following functions are used to check the status of each component of the system. This means that they will check if the component is working properly.
+# FIXME: The following functions are just placeholders. In order to implement these, we need to implement the Uplink first.
+
+def check_motor() -> bool:
+    """Checks the motor status.
+
+    Returns:
+        bool: The motor status.
+    """
+    return bool(np.random.randint(0,2)) # FIXME: This is a placeholder. It should be replaced with the actual value which will be read from the csv file
+
+def check_sound_card() -> bool:
+    """Checks the sound card status.
+
+    Returns:
+        bool: The sound card status.
+    """
+    time.sleep(1) # FIXME: These sleep calls are here for testing purposes. They should be removed when the Uplink is implemented.
+    return bool(np.random.randint(0,2)) # FIXME: This is a placeholder. It should be replaced with the actual value which will be read from the csv file
+
+def check_camera() -> bool:
+    """Checks the camera status.
+
+    Returns:
+        bool: The camera status.
+    """
+    time.sleep(2) # FIXME: These sleep calls are here for testing purposes. They should be removed when the Uplink is implemented.
+    return bool(np.random.randint(0,2)) # FIXME: This is a placeholder. It should be replaced with the actual value which will be read from the csv file
+
+def check_heater() -> bool:
+    """Checks the heater status.
+
+    Returns:
+        bool: The heater status.
+    """
+    time.sleep(3) # FIXME: These sleep calls are here for testing purposes. They should be removed when the Uplink is implemented.
+    return bool(np.random.randint(0,2)) # FIXME: This is a placeholder. It should be replaced with the actual value which will be read from the csv file
+
+def delete_data() -> bool:
+    """Deletes the data from the db.
+    
+    Returns:
+        bool: The status of the operation.
+    """
+    return bool(np.random.randint(0,2)) # FIXME: This is a placeholder. It should be replaced with the actual value which will be read from the csv file
