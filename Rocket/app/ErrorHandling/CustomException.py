f8965f625580737d6cf39648667160b1eadd4057
Hercules Konsoulas <dyka3773@gmail.com>
2023-08-12 22:11:33 +0300

    Added a custom exception wrapper and an error enum that will be used to save any errors occuring during the experiment

---
+++ Rocket/app/ErrorHandling/CustomException.py
@@ -0,0 +1,22 @@
+import sys
+import os
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+# The above code is a hack used to import modules from the parent directory. 
+# NOTE: I DO NOT recommend using this in production code.
+
+from StoreData import StoreData
+from ErrorCode import ErrorCode
+
+class CustomException(Exception):    
+    
+    def __init__(self, message: str, error: ErrorCode):
+        self.message = message
+        self.error = error
+        self.error_code = error.value
+        
+        StoreData().save_error_code(self.error_code)
+
+    def __str__(self):
+        return f'{self.message} (Error code: {self.error_code})'
