405ca816e0339f09c280cd2188f1fb5a6f9a1eca
xrysanthemo <chrysaepiskopou@gmail.com>
2023-10-22 12:54:21 +0300

    test implementation of motor control added

---
+++ Rocket/archive/motor_control_v2.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+
+# Define the GPIO pin number
+gpio_pin = 18
+
+# Set the GPIO mode to BCM
+GPIO.setmode(GPIO.BCM)
+
+# Set up the GPIO pin as an output
+GPIO.setup(gpio_pin, GPIO.OUT)
+
+# Define the frequency in Hz (e.g., 1 Hz)
+frequency = 1
+
+try:
+    while True:
+        # Toggle the GPIO pin high (3.3V)
+        GPIO.output(gpio_pin, GPIO.HIGH)
+        time.sleep(0.5 / frequency)
+
+        # Toggle the GPIO pin low (0V)
+        GPIO.output(gpio_pin, GPIO.LOW)
+        time.sleep(0.5 / frequency)
+
+except KeyboardInterrupt:
+    pass
+
+# Cleanup and release resources
+GPIO.cleanup()
+
