555f28f24ac5e7991b46ae85955b6c630c48d37a
Hercules Konsoulas <dyka3773@gmail.com>
2023-12-10 22:52:05 +0200

    minor updates to the pwm script

--- Rocket/archive/new/pwm.py
+++ Rocket/archive/new/pwm.py
@@ -1,16 +1,4 @@
-################################ pwm_for_servos_example.py ###################################
-# Peter Halverson Nov. 1, 2014
-# The LabJack U3 can output two pulse-width modulated signals.  You could use it to control
-# two servo motors.
-#
-# PWM signal 0 comes out of the FI04 screw terminal.
-# PWM signal 1 comes out of the FI05 screw terminal.  (Strange, but that's the way it is.)
-#
-# Documentation that allowed me to write this code is at
-# http://labjack.com/support/u6/users-guide/2.9.1.1
-# and in u3.py
 import time
-
 import u3
 import logging
 
@@ -22,21 +10,17 @@ logging.basicConfig(
     filemode='a'
 )
 
-device = u3.U3()  # Opens first found U3 over USB
+device = u3.U3()
+
 # d.configIO() # FIXME: Check if this is necessary
+
 device.configIO(NumberOfTimersEnabled=2)
+
 # TODO: Document what these parameters do
 device.configTimerClock(
     TimerClockBase=3,
     TimerClockDivisor=1
 )
-
-
-# This combination selects the 48 MHz timebase.
-# The PWM frequency will be 48 MHz/(15*2^16) which works out to 48.82 Hz, close to
-# the 50 Hz standard for analog servos.
-# Various frequencies are available.  0.06 to 732 Hz in mode 0.  15.26 to 187500 Hz in mode 1
-# See the documentation.
 
 # This example allows the LabJack U3 conrol two analog servos using PWM.
 # The control signals will come out of the FIO4 and FIO5 screw terminals.
