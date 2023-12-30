7e001dac516ec49dcf1654e44115d72b27e045e1
Hercules Konsoulas <dyka3773@gmail.com>
2023-12-30 12:45:52 +0200

    removed some redundant helping scripts that were not used

--- Ground Station/install_project_requirements.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-@REM Installs the project requirements to the current virtual environment
-@REM Assumes that the virtual environment is activated
-
-pip install -r requirements.txt
-
-PAUSE
--- Ground Station/refresh_or_create_project_requirements.bat
+++
@@ -1,7 +0,0 @@
-@echo off 
-
-@REM Refreshes or creates the project requirements file
-
-pip freeze > requirements.txt
-
-PAUSE
