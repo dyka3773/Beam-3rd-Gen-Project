93eec63d405d8afb52555bb249681e5250c53923
Hercules Konsoulas <dyka3773@gmail.com>
2024-02-04 21:16:02 +0200

    Updated Diagrams for the SED and minor changes

--- Ground Station/run_sonarqube_analysis.bat
+++ Ground Station/run_sonarqube_analysis.bat
@@ -3,4 +3,4 @@
 @REM The sonarqube server must be running on the local machine
 @REM The sonarqube server must have the project already created named "Beam-3rd-Gen" and the token must be generated
 @REM This also assumes that the sonar-scanner is in the path
-sonar-scanner.bat -D"sonar.projectKey=Beam-3rd-Gen" -D"sonar.sources=." -D"sonar.host.url=http://localhost:9000" -D"sonar.token=sqp_1a5d3b0120dd61a8f50624deb5264b93c4bb65b6"
+sonar-scanner.bat -D"sonar.projectKey=Beam-3rd-Gen" -D"sonar.sources=." -D"sonar.host.url=http://localhost:9000" -D"sonar.token=sqp_268963a91e3595c3fe7742c1745be4e013733fe8"
